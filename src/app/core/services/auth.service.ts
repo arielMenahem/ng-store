cce11c9e37dfe99fde83278d56e88ba218302fd7
arielMenahem <arielMenah@gmail.com>
2023-03-23 15:30:15 +0200

    add lofin dialog

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
 import { IUser } from 'src/app/shared/models';
+import { StorageService } from 'src/app/core/services/storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  constructor(private storageService: StorageService) {}
+
   public authanticate(user: IUser): boolean {
+    if (user) {
+      this.storageService.setData('user', user);
+    }
     return true;
   }
 }
